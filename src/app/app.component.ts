93daf0518f8b86da46a7c76940db78d429c456e8
Charles maina <muragemaina09@gmail.com>
2019-05-06 12:04:28 +0300

    added gh-pages

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public ngOnInit(){
+    // $(document).ready(function(){
+    //  $('#but0').click(function(){
+    //    $('#land').slideUp()
+    //    $('#det').show();
+    //  })
+    // });
+  }
+}
